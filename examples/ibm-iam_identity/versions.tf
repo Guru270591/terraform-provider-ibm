6fa907f316be8ddfea6a43449c1c9ef6d768f6c7
Guru270591 <35008720+Guru270591@users.noreply.github.com>
2021-08-13 07:40:20 -0700

    Update versions.tf

--- examples/ibm-iam_identity/versions.tf
+++ examples/ibm-iam_identity/versions.tf
@@ -1,8 +1,9 @@
 terraform {
-  required_version = ">= 0.12"
-  required_providers {
+  required_version = ">= 0.13"
+   required_providers {
     ibm = {
       source  = "ibm-cloud/ibm"
       version = "~>1.25.0"
     }
+  }
 }
