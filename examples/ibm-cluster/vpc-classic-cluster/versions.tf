24b19ec678fe1efaa08531dfb09685d13eb38faf
Guru270591 <35008720+Guru270591@users.noreply.github.com>
2021-08-13 07:55:16 -0700

    Update versions.tf

--- examples/ibm-cluster/vpc-classic-cluster/versions.tf
+++ examples/ibm-cluster/vpc-classic-cluster/versions.tf
@@ -1,4 +1,13 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+   required_providers {
+    ibm = {
+      source  = "ibm-cloud/ibm"
+      version = "~>1.25.0"
+    }
+    kubernetes = {
+      version = "~> 1.13.3"
+    }
+  }
 }
